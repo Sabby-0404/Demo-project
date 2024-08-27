c36d95bf24b91f8e63507989f1996034910aceae
Sabby-0404 <shinde04saba@outlook.com>
2024-08-27 14:48:28 +0530

    Update jenkins-config.sh

--- jenkins-config.sh
+++ jenkins-config.sh
@@ -1,11 +1,11 @@
 app_tag=`git ls-remote https://github.com/javahometech/node-app HEAD | awk '{print $1}'`
 
-docker_app="kammana/nodejenkins:$app_tag"
+docker_app="sabby404/demo-docker-repository:$app_tag"
 docker build -t $docker_app .
 
-docker login -u kammana -p yourpassword 
+podman login -u sabby404 -p yourpassword 
 
-docker push $docker_app
+podman push $docker_app
 
 scp -i /var/lib/jenkins/dev.pem deploy.sh ec2-user@172.31.43.90:/tmp
 
